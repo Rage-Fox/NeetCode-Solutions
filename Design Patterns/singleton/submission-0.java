464fecdaa09b1090a18001436df1c2860b44434a
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-06-07 14:52:29 +0530

    Add: singleton - submission-0

---
+++ Design Patterns/singleton/submission-0.java
@@ -0,0 +1,27 @@
+static class Singleton {
+    private String value=null;
+    private static volatile Singleton unqInst=null;
+    private Singleton() {
+        value=null;
+    }
+
+    public static Singleton getInstance() {
+        if(unqInst==null){
+            synchronized(Singleton.class){
+                if(unqInst==null){
+                    unqInst=new Singleton();
+                }
+            }
+        }
+        return unqInst;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value=value;
+    }
+    
+}
